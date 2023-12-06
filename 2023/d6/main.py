52042be429727622b2a42f96d9d931b5312dd802
Matej Ferenc <matej.ferenc@bitstamp.net>
2023-12-06 12:49:22 +0100

    cleanup and optimisation

--- 2023/d6/main.py
+++ 2023/d6/main.py
@@ -8,6 +8,12 @@ distances = [int(x) for x in lines[1].split(':')[-1].split()]
 def calculate(time,record):
     count = 0
     start_point = time//2
+    # # Takes 4sec
+    # for r in range(time):
+    #     if r*(time-r) > record:
+    #         count +=1
+
+    # Takes 3sec
     for x in range(start_point, 0,-1):
         if x*(time-x) > record:
             count +=1
@@ -21,8 +27,8 @@ def calculate(time,record):
     return count
 
 ans = ans1 = 1
-for i, v in enumerate(times):
-    ans *= calculate(v, distances[i])
+for time, distance in zip(times, distances):
+    ans *= calculate(time, distance)
 print(ans)
 
 t = int(''.join(map(str, times)))
